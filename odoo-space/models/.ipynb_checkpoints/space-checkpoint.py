49d5702d083a7817aa9aa31d65db2026b31762e9
MarielSarai <marielsaraigar@gmail.com>
2021-09-27 05:24:48 +0000

    Agregar campos a Nave Spacial

--- odoo-space/models/.ipynb_checkpoints/space-checkpoint.py
+++ odoo-space/models/.ipynb_checkpoints/space-checkpoint.py
@@ -6,3 +6,10 @@ class Space(models.Model):
     
     _name = 'space.odoo'
     _description = 'Space info'
+    
+    name = fields.Char(string='Nombre', required=True)
+    dimension = fields.Char(string='Dimensiones')
+    numpasajeros = fields.Char(string='Pasajeros')
+    tipobarco = fields.Char(string='Tipo de Barco')
+    active = fields.Boolean(string='Active', default=True)
+    
