b60e9a9484fdfd3747aa34f3b158988629b272c4
MarielSarai <marielsaraigar@gmail.com>
2021-09-27 05:49:31 +0000

    Se agrego campos libreria

--- odoo-library/models/library.py
+++ odoo-library/models/library.py
@@ -6,3 +6,10 @@ class Library(models.Model):
     
     _name = 'library.odoo'
     _description = 'Library info'
+    
+    name = fields.Char(string='Nombre del libro', required=True)
+    autor = fields.Char(string='Autor', required=True)
+    editor = fields.Char(string='Editor', required=True)
+    año_edicion = fields.Char(string='Año de edicion', required=True)
+    genero = fields.Char(string='Genero', required=True)
+    
